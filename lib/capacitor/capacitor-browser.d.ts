44e3e42b23ccc56cca31115a588a435af8797466
Florian Nolden <florian.nolden@googlemail.com>
2021-06-17 13:06:59 +0200

    updated libs

--- lib/capacitor/capacitor-browser.d.ts
+++ lib/capacitor/capacitor-browser.d.ts
@@ -1,4 +1,4 @@
-import { Browser } from '../auth-browser';
+import { Browser as Browser } from '../auth-browser';
 export declare class CapacitorBrowser extends Browser {
     closeWindow(): void | Promise<void>;
     showWindow(url: string): Promise<string | undefined>;
